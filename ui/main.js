33728afc9c99f8ffbe2b9031e37e9e3ea65b8fdf
vaibhav-17596 <vaibhav.17596@gmail.com>
2016-10-09 16:32:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,10 @@ console.log('Loaded!');
 //Change the text of the main-text div
 var element=document.getElementById('main-text');
 element.innerHTML='new Value';
+
+//Move the image on clicking
+var img=document.getElementById('nodi');
+img.onclick=function()
+{
+  img.style.marginLeft='100px';  
+};
