b99d7001f987134b003098823bdbda45400a4f45
vaibhav-17596 <vaibhav.17596@gmail.com>
2016-10-09 19:22:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,17 @@
-console.log('Loaded!');
+//Counter Code
+var button=document.getElementById("counter");
+var counter=0;
 
-//Change the text of the main-text div
-var element=document.getElementById('main-text');
-element.innerHTML='new Value';
-
-//Move the image on clicking
-var img=document.getElementById('nodi');
-var marginLeft=0;
-function moveRight()
+button.onclick=function()
 {
-marginLeft+=1;
-img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function()
-{
-    var interval=setInterval(moveRight,50);
-   
+    //Make a request to the counter endpoint
+    
+    //Capture the response & store it in a variable
+    
+    //Render the variable in the correct span
+    
+    counter+=1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
     
 };
