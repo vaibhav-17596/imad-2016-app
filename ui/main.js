d2c4c6f8cd55a1f8a74384cc74bd785fafa6881a
vaibhav-17596 <vaibhav.17596@gmail.com>
2016-10-10 12:37:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,16 +29,37 @@ button.onclick=function()
     
 };
 
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
 var submit=document.getElementById('submit_bttn');
 submit.onclick=function()
 {
-    var names=['name1','name2','name3','name4'];
-    var list='';
-    for(var i=0;i<names.length;i++)
-    {list += '<li>'+names[i]+'</li>'}
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
+    //Create a request object
+    var request=new XMLHTTPRequest();
+    
+    
+    //Capture the response & store it in a variable
+    request.onreadystatechange=function()
+    {
+        if(request.readyState===XMLHttpRequest.DONE)
+        {//Take some action
+        if(request.status===200)
+           {
+        var names=request.responseText;
+        names=JSON.parse(names);
+        var list='';
+        for(var i=0;i<names.length;i++)
+        list='<li>'+names[i]+'</li>';
+        var ul=document.getElementById('namelist');
+        ul.innerHTML=list;
+        
+            }
+        }
+        
+        
+    };
+   //Make a request
+   var nameInput=document.getElementById('name');
+var name=nameInput.value;
+  request.open('GET','http://vaibhav-17596.imad.hasura-app.io/submit-name?name=',name,true);
+  request.send(null);
     
 };
