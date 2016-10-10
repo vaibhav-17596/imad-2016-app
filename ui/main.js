a4c92741d8d2ab03d542c9601f8d63ddf15e6838
vaibhav-17596 <vaibhav.17596@gmail.com>
2016-10-10 10:04:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 //Counter Code
-var button=document.getElementById("counter");
+var button=document.getElementById('counter');
 
 button.onclick=function()
 {
@@ -13,11 +13,11 @@ button.onclick=function()
         if(request.readyState===XMLHttpRequest.DONE)
         {//Take some action
         if(request.status===200)
-        {
+           {
         var counter=request.responseText;
         var span=document.getElementById('count');
         span.innerHTML=counter.toString();
-        }
+            }
         }
         
         
